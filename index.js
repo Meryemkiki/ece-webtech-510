28009a7bb265e6d1c90ccebfcdb46e7fa3060c2d
MJAHED7 <noura.mjahed@hotmail.com>
2023-10-17 07:31:44 +0200

    feat: add new API endpoint for creating articles before refactoring in Rest

--- index.js
+++ index.js
@@ -1,82 +1,100 @@
-/*const http = require('http');
-const url = require('url');
-const fs = require('fs');
-
-const serverHandle = function (req, res) {
-  const route = url.parse(req.url);
-  const path = route.pathname;
-
-  if (path === '/') {
-    // Route pour la page d'accueil (comme dans la partie 5)
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Bienvenue sur la page d\'accueil!');
-  } else if (path === '/hello') {
-    // Route pour la page /hello (comme dans la partie 5)
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, webtech-510');
-  } else if (path === '/about') {
-    // Nouvelle route pour la page /about
-    const filePath = './content/about.json';
-
-    // Lire le fichier JSON de manière asynchrone
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Erreur interne du serveur');
-        return;
-      }
-
-      // Parse le contenu JSON
-      const jsonData = JSON.parse(data);
-
-      // Répond avec le contenu JSON
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(jsonData, null, 2));
-    });
-  } else {
-    // Gestion de page non trouvée (comme dans la partie 5)
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Page non trouvée');
-  }
-};
-
-http.createServer(serverHandle).listen(8080);*/
 const express = require('express');
-const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 const app = express();
-const PORT = 8080;
+
+// Base de données fictive
+const db = {
+  articles: [
+    {
+      id: '6ec0bd7f-11c0-43da-975e-2a8ad9ebae0b',
+      title: 'Mon article',
+      content: 'Contenu de l"article.',
+      date: '04/10/2022',
+      author: 'Liz Gringer'
+    }
+  ],
+  comments: [
+    {
+      id: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
+      timestamp: 1664835049,
+      content: 'Contenu du commentaire.',
+      articleId: '6ec0bd7f-11c0-43da-975e-2a8ad9ebae0b',
+      author: 'Bob McLaren'
+    }
+  ]
+}
+ 
+// Middleware pour lire le JSON
+app.use(express.json());
 
 // Route pour la page d'accueil
 app.get('/', (req, res) => {
   res.send('Bienvenue sur la page d\'accueil!');
 });
 
-// Route pour la page /hello
+// Route pour dire bonjour
 app.get('/hello', (req, res) => {
   res.send('Hello, webtech-510');
 });
 
-// Route pour la page /about
+// Route pour afficher le contenu du fichier about.json
 app.get('/about', (req, res) => {
+  const fs = require('fs');
   const filePath = './content/about.json';
-
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
       res.status(500).send('Erreur interne du serveur');
       return;
     }
-
-    const jsonData = JSON.parse(data);
-    res.json(jsonData);
+    res.json(JSON.parse(data));
   });
 });
 
-// Gestion de page non trouvée
-app.use((req, res) => {
-  res.status(404).send('Page non trouvée');
+// Route pour lister tous les articles
+app.get('/articles', (req, res) => {
+  res.json(db.articles);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// Route pour ajouter un nouvel article
+app.post('/articles', (req, res) => {
+  const { title, content, author, date } = req.body;
+  const newArticle = {
+    id: uuidv4(),
+    title,
+    content,
+    author,
+    date
+  };
+  db.articles.push(newArticle);
+  res.json(newArticle);
 });
+
+// Route pour lister les commentaires d'un article
+app.get('/articles/:articleId/comments', (req, res) => {
+  const articleId = req.params.articleId;
+  const comments = db.comments.filter(c => c.articleId === articleId);
+  res.json(comments);
+});
+
+// Route pour ajouter un commentaire à un article
+app.post('/articles/:articleId/comments', (req, res) => {
+  const articleId = req.params.articleId;
+  const { content, author } = req.body;
+  const timestamp = Date.now();
+  const newComment = {
+    id: uuidv4(),
+    content,
+    articleId,
+    author,
+    timestamp
+  };
+  db.comments.push(newComment);
+  res.json(newComment);
+});
+
+// Lancement du serveur
+app.listen(8080, () => {
+  console.log('Server is running on http://localhost:8080');
+});
+
 
