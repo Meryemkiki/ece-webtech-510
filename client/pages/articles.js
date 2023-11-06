5d3ce5c07d5c04a6126877e4d9d80ec5cc689b54
Meryemkiki <130454453+Meryemkiki@users.noreply.github.com>
2023-11-06 10:32:25 +0100

    Update articles.js

---
+++ client/pages/articles.js
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+import MyAPP from './app';
+
+export default function About(){
+  return(
+    <>
+      <div>
+        <h1 className= text-3xl font-bold mb-4> About Us </h1>
+    </div>
+    </>
+  )
+}
