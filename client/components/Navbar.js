95c7360b6f62e768c321bb8fcdb33f7e5fd5c107
Meryem <meryem.khias@edu.ece.fr>
2023-11-12 18:04:08 +0100

    Add Navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,27 +1,26 @@
 // Navbar.js
 import React from 'react';
-import Link from 'next/link';
+import Logo from '../assets/Logo.png';
+import {Link} from "react-router-dom";
+import'../styles/Navbar.css';
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link href="/">Accueil</Link>
-        </li>
-        <li>
-          <Link href="/about">À propos</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link href="/articles/[articleId]">Articles</Link>
-        </li>
-        {/* Ajoutez d'autres liens de navigation si nécessaire */}
-      </ul>
-    </nav>
-  );
+    <div className='navbar'>
+      <div classeName = "leftSide">
+        <img src = {Logo}/>
+      </div>
+
+      <div classeName = "rightSide">
+        <Link to ="/">Home</Link>
+        <Link to ="/menu">Menu</Link>
+        <Link to ="/about">About us</Link>
+        <Link to ="/contact">Contact</Link>
+      </div>
+    </div>
+
+ )  
+      
 }
 
 export default Navbar;
