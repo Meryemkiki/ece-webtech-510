74f6329b1c316581b11196ffe833c1e5ab70b3d0
Meryem <meryem.khias@edu.ece.fr>
2023-11-12 19:10:49 +0100

    Update

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,14 +1,26 @@
 // Navbar.js
-import React from 'react';
+import React , {useState} from 'react';
 import Logo from '../assets/Logo.png';
 import {Link} from "react-router-dom";
 import'../styles/Navbar.css';
 
 function Navbar() {
+
+    const [openLinks, setOpenLinks] = useState(false) ;
+    const toggleNavbar = () => {
+      setOpenLinks(!openLinks);
+    };
   return (
     <div className='navbar'>
-      <div classeName = "leftSide">
+      <div classeName = "leftSide" id = {openLinks ? "open" : "close"}>
         <img src = {Logo}/>
+        <div className='hiddenLinks'>
+        <Link to ="/">Home</Link>
+        <Link to ="/menu">Menu</Link>
+        <Link to ="/about">About us</Link>
+        <Link to ="/contact">Contact</Link>
+
+        </div>
       </div>
 
       <div classeName = "rightSide">
@@ -16,6 +28,9 @@ function Navbar() {
         <Link to ="/menu">Menu</Link>
         <Link to ="/about">About us</Link>
         <Link to ="/contact">Contact</Link>
+        <button onClick={toggleNavbar}>
+          <ReorderIcon />
+        </button>
       </div>
     </div>
 
--- client/pages/Home.js
+++ client/pages/Home.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import {Link} from "react-router-dom";
+import HomePic from "../assests/homepic.jpeg";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div classeName ="home">
+      <div classeName = "headerContainer" style ={ {backgroundImage : 'url(${HomePic})'}}>
+        <h1>Wanderlust Explorer</h1>
+        <p> Virtually Explore the World!</p>
+        <button> EXPLORE</button>
+      </div>
+    </div>
+  );
 }
 
 export default Home
