0e5a78f8f0756b820e9519703cd5b5036e6707c5
MJAHED7 <noura.mjahed@hotmail.com>
2023-12-29 19:58:39 +0100

    modification des pages

--- client/components/main/pages/main-about-page.tsx
+++ client/components/main/pages/main-about-page.tsx
@@ -15,26 +15,6 @@ const MainAboutPage = () => {
             {mainPageAboutConfig.general.description}
           </p>
         </div>
-        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
-          <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-            {mainPageAboutConfig.features.map((feature) => (
-              <div key={feature.name} className="relative pl-16">
-                <dt className="text-base font-semibold leading-7 text-gray-900">
-                  <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-sky-400">
-                    <feature.icon
-                      className="h-6 w-6 text-white"
-                      aria-hidden="true"
-                    />
-                  </div>
-                  {feature.name}
-                </dt>
-                <dd className="mt-2 text-base leading-7 text-gray-600">
-                  {feature.description}
-                </dd>
-              </div>
-            ))}
-          </dl>
-        </div>
       </div>
     </div>
   );
--- client/config/main/pages/main-page-about-config.ts
+++ client/config/main/pages/main-page-about-config.ts
@@ -11,15 +11,15 @@ const mainPageAboutConfig: AboutPageType = {
     title: "About",
     subTitle: "Fullstack Blogging application",
     description:
-      "This is a fullstack blogging application built with Next.js 13 and Supabase.",
+      "This is a fullstack blogging application built with Next.js 14 and Supabase.",
     paragraphs: [
       {
         description:
-          "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.",
+          "",
       },
       {
         description:
-          "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua. Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.",
+          "",
       },
     ],
   },
